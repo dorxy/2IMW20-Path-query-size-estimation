69452feca0d3e688d02de52c5508282fccad50a4
Simone Alers <simone@dorxy.nl>
2016-06-19 10:50:14 +0200

    Create recursive total size function

--- estimators.py
+++ estimators.py
@@ -1,6 +1,47 @@
 from abc import ABCMeta, abstractmethod
 from itertools import combinations
-import sys
+from sys import getsizeof
+from itertools import chain
+from collections import deque
+
+
+# Source: http://code.activestate.com/recipes/577504-compute-memory-footprint-of-an-object-and-its-cont/
+def total_size(o, handlers={}):
+    """ Returns the approximate memory footprint an object and all of its contents.
+
+    Automatically finds the contents of the following builtin containers and
+    their subclasses:  tuple, list, deque, dict, set and frozenset.
+    To search other containers, add handlers to iterate over their contents:
+
+        handlers = {SomeContainerClass: iter,
+                    OtherContainerClass: OtherContainerClass.get_elements}
+
+    """
+    dict_handler = lambda d: chain.from_iterable(d.items())
+    all_handlers = {tuple: iter,
+                    list: iter,
+                    deque: iter,
+                    dict: dict_handler,
+                    set: iter,
+                    frozenset: iter,
+                    }
+    all_handlers.update(handlers)  # user handlers take precedence
+    seen = set()  # track which object id's have already been seen
+    default_size = getsizeof(0)  # estimate sizeof object without __sizeof__
+
+    def sizeof(o):
+        if id(o) in seen:  # do not double count the same object
+            return 0
+        seen.add(id(o))
+        s = getsizeof(o, default_size)
+
+        for typ, handler in all_handlers.items():
+            if isinstance(o, typ):
+                s += sum(map(sizeof, handler(o)))
+                break
+        return s
+
+    return sizeof(o)
 
 
 # Abstract class for estimators, each estimator
@@ -23,7 +64,7 @@ class Abstract:
         return self._summary
 
     def summary_size(self):
-        return sys.getsizeof(self._summary)
+        return total_size(self._summary)
 
 
 # BruteForce estimator
@@ -143,14 +184,6 @@ class Language(Abstract):
                 return 0
             total *= self._summary['table'][path[i - 1]][path[i]]
         return total
-
-    def summary_size(self):
-        size = sys.getsizeof(self._summary)
-        for sub in self._summary:
-            size += sys.getsizeof(sub)
-        for sub in self._summary['table']:
-            size += sys.getsizeof(sub)
-        return size
 
 
 class Four(Abstract):
